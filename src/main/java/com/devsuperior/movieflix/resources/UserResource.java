01c4b697f70a4bf54dcc8c88963950a866ad61ec
Vitor Battestin <vitor.battestin@gmail.com>
2021-07-01 01:53:35 -0300

    Implementing TDD for tests pass

---
+++ src/main/java/com/devsuperior/movieflix/resources/UserResource.java
@@ -0,0 +1,24 @@
+package com.devsuperior.movieflix.resources;
+
+import com.devsuperior.movieflix.dto.UserDTO;
+import com.devsuperior.movieflix.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+    @Autowired
+    private UserService service;
+
+    @GetMapping(value = "/profile")
+    public ResponseEntity<UserDTO> findProfileUserLogged(){
+        UserDTO dto = service.findProfileUserLogged();
+        return ResponseEntity.ok().body(dto);
+    }
+}
